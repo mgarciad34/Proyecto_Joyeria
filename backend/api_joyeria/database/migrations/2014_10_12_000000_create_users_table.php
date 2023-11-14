b55447531a6daf30a784979c40533f52e5dc21eb
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2023-11-14 18:42:00 +0100

    users-rol

--- backend/api_joyeria/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/api_joyeria/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('rol');
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+  
         });
     }
 
