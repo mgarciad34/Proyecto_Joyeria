89c5ff91c4b9886e3e218aa37c454a460d2c1f3b
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2023-11-18 13:43:46 +0100

    listar joyas en tabla

---
+++ frontend/src/js/appListadoJoyas.js
@@ -0,0 +1,57 @@
+import{obtenerJoyas} from './http/http-listadoJoyas.js'
+
+
+obtenerJoyas().then(function(data){
+    pintarJoyas(data)
+})
+
+
+function pintarJoyas(joyas) {
+    var tabla = document.getElementById('tabla_joyas');
+        console.log(joyas)
+        for(let i=0;i<joyas[0].length;i++) {
+            console.log(joyas[0][i])
+            let fila = document.createElement('tr');
+
+            let botonCelda = document.createElement('td');
+            let boton = document.createElement('button');
+            boton.textContent = 'recetas'
+            boton.setAttribute('id',joyas[0][i].id)
+
+            let idCelda = document.createElement('td');
+
+            let id= document.createElement('span');
+            id.textContent=joyas[0][i].id
+           
+
+            let nombreCelda = document.createElement('td');
+            let nombre= document.createElement('span');
+            nombre.textContent=joyas[0][i].nombre
+           
+
+            let fotoCelda = document.createElement('td');
+            let foto= document.createElement('span');
+            foto.textContent=joyas[0][i].foto
+
+
+            boton.addEventListener('click', function(event) {
+                sessionStorage.setItem('joya-guardada',JSON.parse(boton.id))
+                window.location.href='./detalle-joya.html'
+                
+              });
+
+            idCelda.appendChild(id)
+            nombreCelda.appendChild(nombre)
+            fotoCelda.appendChild(foto)
+           botonCelda.appendChild(boton);
+
+           fila.appendChild(idCelda);
+           fila.appendChild(nombreCelda);
+           fila.appendChild(fotoCelda)
+            fila.appendChild(botonCelda);
+
+            tabla.appendChild(fila);
+        }
+        
+        
+    }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,11 @@ module.exports = {
         appClasificador: './frontend/src/js/appClasificador.js',
         appLote: './frontend/src/js/appLote.js',
         appDesignjoya: './frontend/src/js/appDesignJoya.js',
+        appListadoJoyas: './frontend/src/js/appListadoJoyas.js',
         httpDesignJoya: './frontend/src/js/http/http-designJoya.js',
+        httpClasificador: './frontend/src/js/http/http-Clasificador.js',
+        httpLote: './frontend/src/js/http/http-lote.js',
+        httpListadoJoyas: './frontend/src/js/http/http-listadoJoyas.js',
       },
       output: {
         filename: '[name].bundle.js',
