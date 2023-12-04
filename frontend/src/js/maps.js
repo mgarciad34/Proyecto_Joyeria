bc74175cd196dc83b8560da8335db38012bac0dd
Manuel Garcia <m.gd@outlook.es>
2023-12-04 18:18:49 +0100

    Creamos mapa google

--- frontend/src/js/maps.js
+++ frontend/src/js/maps.js
@@ -1,11 +1,26 @@
+// Initialize and add the map
 let map;
 
 async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
+  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    center: { lat: 38.6929488, lng: -4.1111371 },
-    zoom: 16,
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
   });
 }
 
