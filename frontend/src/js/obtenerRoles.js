1220afb974ffe1ae7709eca01b4a4f50fda15083
Manuel Garcia <m.gd@outlook.es>
2023-12-07 14:01:06 +0100

    agregamos funcionalidad roles

--- frontend/src/js/obtenerRoles.js
+++ frontend/src/js/obtenerRoles.js
@@ -1,19 +1,64 @@
-import { obtenerRoles} from "./http/http-ObtenerRoles.js";
+import { obtenerRoles } from "./http/http-ObtenerRoles.js";
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var rolesDiv = document.getElementById('roles');
     if (rolesDiv) {
-        var rolesAsignados = {};
         var id = sessionStorage.getItem("id-usuario");
-       obtenerRoles(id)
-        .then(result => {
-            rolesAsignados[id] = result;
-            console.log(rolesAsignados)
-            
-        })
-        .catch(error => {
-            console.error('Error al obtener roles:', error);
-        });
+
+        obtenerRoles(id)
+            .then(result => {
+                // Verificar si result es un objeto con una clave numérica
+                if (result && typeof result === 'object' && Object.keys(result).length === 1) {
+                    // Obtener la primera clave numérica del objeto (asumiendo que solo hay una)
+                    var id_usuario = Object.keys(result)[0];
+
+                    if (Array.isArray(result[id_usuario])) {
+                        result[id_usuario].forEach(rol => {
+                            const boton = document.createElement('button');
+                            boton.className = "btn status cancelled";
+                            switch (rol.id_rol) {
+                                case 1:
+                                    boton.textContent = "Administrador";
+                                    break;
+                                case 2:
+                                    boton.textContent = "Clasificador";
+                                    break;
+                                case 3:
+                                    boton.textContent = "Diseñador";
+                                    break;
+                                case 4:
+                                    boton.textContent = "Colaborador";
+                                    break;
+                            }
+
+                            boton.addEventListener('click', () => {
+                                switch (rol.id_rol) {
+                                    case 1:
+                                        window.location.href = './indexAdministrador.html';
+                                        break;
+                                    case 2:
+                                        window.location.href = './indexClasificador.html';
+                                        break;
+                                    case 3:
+                                        window.location.href = './listaJoyas.html';
+                                        break;
+                                    case 4:
+                                        window.location.href = './pantalla-rol-2.html';
+                                        break;
+                                }
+                            });
+                            rolesDiv.appendChild(boton);
+                        });
+                    } else {
+                        console.error('No es un array de roles:', result[id_usuario]);
+                    }
+                } else {
+                    console.error('No es un objeto', result);
+                }
+            })
+            .catch(error => {
+                console.error('Error al obtener roles:', error);
+            });
     } else {
         console.error('No se encontró el div de roles');
     }
