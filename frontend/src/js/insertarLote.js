818d06bfdf7db891a263d65881b5ba483a2808b0
Manuel Garcia <m.gd@outlook.es>
2023-12-07 11:55:32 +0100

    modificando funcionalidad insertar lote

--- frontend/src/js/insertarLote.js
+++ frontend/src/js/insertarLote.js
@@ -1,46 +1,15 @@
 //Manuel
-import { obtenerDatos } from '../js/http/http-consultarUsuarios.js';
 import AnadirLote from './clases/Lote.js';
 import { enviarDatos } from './http/http-insertarRol.js';
 
 var btnRegistrar = document.getElementById('btnAnadirLote');
 
-document.addEventListener('DOMContentLoaded', function () {
-    var selUsuario = document.getElementById('selUsuario');
 
-    if (!selUsuario) {
-        console.error('No se encontró el elemento con ID "selUsuario".');
-        return;
-    }
-
-    obtenerDatos()
-        .then(response => {
-            const usuarios = response.usuarios;
-
-            if (Array.isArray(usuarios)) {
-                // Limpiar las opciones actuales del select
-                selUsuario.innerHTML = '';
-
-                // Agregar una opción por cada usuario
-                usuarios.forEach(usuario => {
-                    var optionUsuario = document.createElement('option');
-                    optionUsuario.value = usuario.id;
-                    optionUsuario.text = usuario.name;
-                    selUsuario.add(optionUsuario);
-                });
-            } else {
-                console.error('La respuesta de la API no es un array:', usuarios);
-            }
-        })
-        .catch(error => {
-            console.error('Error al cargar datos en el select:', error);
-        });
-});
 
 btnRegistrar.addEventListener('click', function(event){
     event.preventDefault();
     console.log(sessionStorage.getItem("Latitud"))
-    var anadirLote = new AnadirLote(selUsuario.value, sessionStorage.getItem("Latitud"), sessionStorage.getItem("Longitud"), 'Entregado');
+    var anadirLote = new AnadirLote(sessionStorage.getItem("id-usuario"), sessionStorage.getItem("Latitud"), sessionStorage.getItem("Longitud"), 'Entregado');
     enviarDatos(anadirLote, 'http://127.0.0.1:8000/api/lotes/agregar/lote');
     window.location.href="indexColaborador.html";
 
