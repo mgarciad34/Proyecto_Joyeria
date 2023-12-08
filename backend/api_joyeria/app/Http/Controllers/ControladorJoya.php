c5c018f0163ab81167ea119846c7fc852f33c3b4
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2023-12-08 19:49:20 +0100

    recomendador- servicio y ruta

--- backend/api_joyeria/app/Http/Controllers/ControladorJoya.php
+++ backend/api_joyeria/app/Http/Controllers/ControladorJoya.php
@@ -19,8 +19,9 @@ use Illuminate\Support\Facades\Validator;
 class ControladorJoya extends Controller
 {
     /**Óscar */
-    function nuevaJoya(Request $request){
-        try{
+    function nuevaJoya(Request $request)
+    {
+        try {
             $mensajes = [
                 'nombre.required' => 'El campo nombre es obligatorio.',
                 'nombre.string' => 'El campo nombre debe ser una cadena de texto.',
@@ -34,8 +35,8 @@ class ControladorJoya extends Controller
                 'detalle.*.cantidad.required' => 'El campo cantidad en detalle es obligatorio.',
                 'detalle.*.cantidad.integer' => 'El campo cantidad en detalle debe ser un número entero.',
             ];
-    
-           
+
+
             $validator = Validator::make($request->all(), [
                 'nombre' => 'required|string|max:255',
                 'id_usuario' => 'required|integer',
@@ -43,54 +44,54 @@ class ControladorJoya extends Controller
                 'detalle.*.tipo' => 'required|integer',
                 'detalle.*.cantidad' => 'required|integer',
             ], $mensajes);
-    
+
             if ($validator->fails()) {
                 return response()->json(['mensaje' => 'Error en la validación', 'errores' => $validator->errors()], 400);
             }
 
-            $joya=new Joya();
-            $joya->nombre=$request->get('nombre');
+            $joya = new Joya();
+            $joya->nombre = $request->get('nombre');
             // $joya->foto=$request->get('foto');
-            $joya->id_usuario=$request->get('id_usuario');
+            $joya->id_usuario = $request->get('id_usuario');
             $joya->save();
-            $joya->foto="https://jawa-oscar.s3.eu-west-3.amazonaws.com/joyas/".$joya->id;
+            $joya->foto = "https://jawa-oscar.s3.eu-west-3.amazonaws.com/joyas/" . $joya->id;
             $joya->save();
-            $detalle=$request->detalle;
-            
-            for ($i=0;$i<count($detalle);$i++) {
-                
-                $componente = new Detalle_receta();
-                $componente->id_joya=$joya->id;
-                $componente->id_componente=$detalle[$i]['tipo'];
-                $componente->cantidad=$detalle[$i]['cantidad'];
-                $componente->save();
-                
-                
-            }
-            
-            return response()->json(['mensaje'=>'Nueva joya registrada correctamente','id'=>$joya->id],201);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al crear la joya'],400);
-        }
-    }
-    /**Óscar */
-    function getAllJoyas(){
-        try{
+            $detalle = $request->detalle;
 
-            $joyas=Joya::all();
-            for($i=0;$i<count($joyas);$i++){
-                $usuario=User::find($joyas[$i]->id_usuario);
-                $joyas[$i]->creador=$usuario->name;
+            for ($i = 0; $i < count($detalle); $i++) {
+
+                $componente = new Detalle_receta();
+                $componente->id_joya = $joya->id;
+                $componente->id_componente = $detalle[$i]['tipo'];
+                $componente->cantidad = $detalle[$i]['cantidad'];
+                $componente->save();
             }
-           
-            return response()->json([$joyas],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al obtener las joyas'],500);
+
+            return response()->json(['mensaje' => 'Nueva joya registrada correctamente', 'id' => $joya->id], 201);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al crear la joya'], 400);
         }
     }
     /**Óscar */
-    function fabricarJoya($id, Request $request){
-        try{
+    function getAllJoyas()
+    {
+        try {
+
+            $joyas = Joya::all();
+            for ($i = 0; $i < count($joyas); $i++) {
+                $usuario = User::find($joyas[$i]->id_usuario);
+                $joyas[$i]->creador = $usuario->name;
+            }
+
+            return response()->json([$joyas], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener las joyas'], 500);
+        }
+    }
+    /**Óscar */
+    function fabricarJoya($id, Request $request)
+    {
+        try {
             $mensajes = [
                 'id_usuario.required' => 'El campo :attribute es obligatorio.',
                 'id_usuario.integer' => 'El campo :attribute debe ser un número entero.',
@@ -98,88 +99,91 @@ class ControladorJoya extends Controller
             $validator = Validator::make($request->all(), [
                 'id_usuario' => 'required|integer',
             ], $mensajes);
-    
+
             if ($validator->fails()) {
                 return response()->json(['mensaje' => 'Error en la validación', 'errores' => $validator->errors()], 400);
             }
 
-            $historico= new HistoricoJoya();
-            $historico->id_joya=$id;
-            $historico->id_usuario=$request->get('id_usuario');
-            $historico->creado=now();
+            $historico = new HistoricoJoya();
+            $historico->id_joya = $id;
+            $historico->id_usuario = $request->get('id_usuario');
+            $historico->creado = now();
             $historico->save();
-            $receta=Detalle_receta::where('id_joya','=',$id)->get();
+            $receta = Detalle_receta::where('id_joya', '=', $id)->get();
             print_r($receta[0]->id_componente);
-            for($i=0;$i<count($receta);$i++){
-                $tipo=Tipos_componente::find($receta[$i]->id_componente);
-                $tipo->cantidad-=$receta[$i]->cantidad;
+            for ($i = 0; $i < count($receta); $i++) {
+                $tipo = Tipos_componente::find($receta[$i]->id_componente);
+                $tipo->cantidad -= $receta[$i]->cantidad;
                 $tipo->save();
             }
-            return response()->json(['mensaje'=>'Fabricado correctamente'],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al fabricar la joya'],404);
+            return response()->json(['mensaje' => 'Fabricado correctamente'], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al fabricar la joya'], 404);
         }
     }
-/**Óscar */
-    function getJoyaFromUsuario($id){
-        try{
+    /**Óscar */
+    function getJoyaFromUsuario($id)
+    {
+        try {
 
-            $joyas=Joya::where('id_usuario','=',$id)->get();
-            return response()->json([$joyas],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al obtener la joya del usuario'],404);
+            $joyas = Joya::where('id_usuario', '=', $id)->get();
+            return response()->json([$joyas], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener la joya del usuario'], 404);
         }
     }
-/**Óscar */
-    function getAllHistorial(){
-        try{
+    /**Óscar */
+    function getAllHistorial()
+    {
+        try {
 
-            $historico['h']=HistoricoJoya::all();
-            $enviar=[];
-            for($i=0;$i<count($historico['h']);$i++){
-                $joya=Joya::find($historico['h'][$i]->id_joya);
-                if($joya){
-                    $usuario=User::find($historico['h'][$i]->id_usuario);
-                    $historico['h'][$i]->creador=$usuario->name;
-                    $historico['h'][$i]->nombre_joya=$joya->nombre;
-                    $historico['h'][$i]->foto=$joya->foto;
-                    $enviar['historial'][]=$historico['h'][$i];
-                }else{
-                    
+            $historico['h'] = HistoricoJoya::all();
+            $enviar = [];
+            for ($i = 0; $i < count($historico['h']); $i++) {
+                $joya = Joya::find($historico['h'][$i]->id_joya);
+                if ($joya) {
+                    $usuario = User::find($historico['h'][$i]->id_usuario);
+                    $historico['h'][$i]->creador = $usuario->name;
+                    $historico['h'][$i]->nombre_joya = $joya->nombre;
+                    $historico['h'][$i]->foto = $joya->foto;
+                    $enviar['historial'][] = $historico['h'][$i];
+                } else {
                 }
-             
             }
-             return response()->json([$enviar],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al obtener el historial','error'=>$e->getMessage()],500);
+            return response()->json([$enviar], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener el historial', 'error' => $e->getMessage()], 500);
         }
     }
     /**Óscar */
-    function deleteJoya($id){
-        try{
+    function deleteJoya($id)
+    {
+        try {
 
-            $joya=Joya::find($id);
+            $joya = Joya::find($id);
             $joya->delete();
-            return response()->json(['mensaje'=>'Eliminado correctamente'],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al eliminar la joya'],404);
+            return response()->json(['mensaje' => 'Eliminado correctamente'], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al eliminar la joya'], 404);
         }
     }
     /**Óscar */
-    function getJoyaById($id){
-        try{
+    function getJoyaById($id)
+    {
+        try {
 
             $joya = Joya::findOrFail($id);
-          
-            return response()->json($joya,200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al obtener la joya'],404);
+
+            return response()->json($joya, 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener la joya'], 404);
         }
     }
     /**Óscar */
-    function updateJoya($id,Request $request){
-        try{
-          
+    function updateJoya($id, Request $request)
+    {
+        try {
+
             $mensajes = [
                 'joya_original.nombre.required' => 'El campo :attribute es obligatorio.',
                 'joya_original.nombre.string' => 'El campo :attribute debe ser una cadena de texto.',
@@ -192,55 +196,55 @@ class ControladorJoya extends Controller
                 'joya.detalle.required' => 'El campo :attribute es obligatorio.',
                 'joya.detalle.array' => 'El campo :attribute debe tener un formato correcto.',
             ];
-    
-            $validator = Validator::make($request->all(),[
+
+            $validator = Validator::make($request->all(), [
                 'joya_original.nombre' => 'required|string|max:255',
                 'joya.nombre' => 'required|string|max:255',
                 'joya_original.detalle' => 'required|array',
                 'joya.detalle' => 'required|array',
             ], $mensajes);
-    
-       
+
+
             if ($validator->fails()) {
                 return response()->json(['mensaje' => 'Error en la validación', 'errores' => $validator->errors()], 400);
             }
 
-            $joya=Joya::find($id);
-            $detalle=Detalle_receta::where('id_joya','=',$id)->get();
-            $joyaOriginal=$request->get('joya_original');
-            $joyaUpdate=$request->get('joya');
-            
-            if($joyaOriginal['nombre']!=$joyaUpdate['nombre']){
-                $joya->nombre=$joyaUpdate['nombre'];
-                
+            $joya = Joya::find($id);
+            $detalle = Detalle_receta::where('id_joya', '=', $id)->get();
+            $joyaOriginal = $request->get('joya_original');
+            $joyaUpdate = $request->get('joya');
+
+            if ($joyaOriginal['nombre'] != $joyaUpdate['nombre']) {
+                $joya->nombre = $joyaUpdate['nombre'];
             }
             $joya->save();
-            if($joyaUpdate['detalle']!=$joyaOriginal['detalle']){
-                $c=new ControladorRec;
-                $c->updateReceta($id,$joyaOriginal['detalle'],$joyaUpdate['detalle']);
+            if ($joyaUpdate['detalle'] != $joyaOriginal['detalle']) {
+                $c = new ControladorRec;
+                $c->updateReceta($id, $joyaOriginal['detalle'], $joyaUpdate['detalle']);
             }
-            return response()->json(['mensaje'=>'Actualizado correctamente'],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al actualizar la joya'],400);
+            return response()->json(['mensaje' => 'Actualizado correctamente'], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al actualizar la joya'], 400);
         }
-            
     }
     /**Óscar */
-    function ownerJoya($id,$id_usuario){
-       try{
+    function ownerJoya($id, $id_usuario)
+    {
+        try {
 
-           $cuenta=Joya::where('id','=',$id)->where('id_usuario','=',$id_usuario)->count();
-           return response()->json(['resultado'=>$cuenta>0],200);
-        }catch(Exception $e){
-            return response()->json(['mensaje'=>'Error al obtener el resultado'],400);
+            $cuenta = Joya::where('id', '=', $id)->where('id_usuario', '=', $id_usuario)->count();
+            return response()->json(['resultado' => $cuenta > 0], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener el resultado'], 400);
         }
-        }
-/**Óscar */
-    function getDisponibles(){
-        try{
+    }
+    /**Óscar */
+    function getDisponibles()
+    {
+        try {
 
-            $disponibles=[];
-            $joyas=DB::select('SELECT j.id AS id_joya,
+            $disponibles = [];
+            $joyas = DB::select('SELECT j.id AS id_joya,
             FLOOR(MIN(tc.cantidad / dr.cantidad))
             AS veces_fabricacion FROM joyas j
             JOIN detalle_recetas dr ON j.id = dr.id_joya
@@ -249,18 +253,78 @@ class ControladorJoya extends Controller
             (SELECT COUNT(*) FROM detalle_recetas WHERE id_joya = j.id)
             AND MIN(tc.cantidad / dr.cantidad)>=1
         ');
-      for ($i=0;$i<count($joyas);$i++){
-          $joya=Joya::find($joyas[$i]->id_joya);
-          $usuario=User::find($joya->id_usuario);
-          $joya->creador=$usuario->name;
-          $joya->fabricaciones=$joyas[$i]->veces_fabricacion;
-          $disponibles[]=$joya;
-          
+            for ($i = 0; $i < count($joyas); $i++) {
+                $joya = Joya::find($joyas[$i]->id_joya);
+                $usuario = User::find($joya->id_usuario);
+                $joya->creador = $usuario->name;
+                $joya->fabricaciones = $joyas[$i]->veces_fabricacion;
+                $disponibles[] = $joya;
+            }
+            return response()->json([$disponibles], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener las joyas fabricables'], 500);
         }
-        return response()->json([$disponibles],200);
-    }catch(Exception $e){
-        return response()->json(['mensaje'=>'Error al obtener las joyas fabricables'],500);
     }
+    function getRecomendaciones($parametro)
+    {
+        try {
 
+            $disponibles = [];
+            if ($parametro == 4) {
+
+                $disponibles = [];
+            $joyas = DB::select('SELECT j.id AS id_joya,
+            FLOOR(MIN(tc.cantidad / dr.cantidad))
+            AS veces_fabricacion FROM joyas j
+            JOIN detalle_recetas dr ON j.id = dr.id_joya
+            JOIN tipos_componentes tc ON dr.id_componente = tc.id
+            GROUP BY j.id HAVING COUNT(dr.id_componente) =
+            (SELECT COUNT(*) FROM detalle_recetas WHERE id_joya = j.id)
+            AND MIN(tc.cantidad / dr.cantidad)>=1 order by id_joya desc
+        ');
+            }
+            if ($parametro == 3) {
+
+                $joyas = DB::select('SELECT j.id AS id_joya,
+                FLOOR(MIN(tc.cantidad / dr.cantidad))
+                AS veces_fabricacion FROM joyas j
+                JOIN detalle_recetas dr ON j.id = dr.id_joya
+                JOIN tipos_componentes tc ON dr.id_componente = tc.id
+                GROUP BY j.id HAVING COUNT(dr.id_componente) =
+                (SELECT COUNT(*) FROM detalle_recetas WHERE id_joya = j.id)
+                AND MIN(tc.cantidad / dr.cantidad)>=1
+                order BY veces_fabricacion DESC
+        ');
+            }
+            if ($parametro == 2) {
+
+                $joyas = DB::select('SELECT j.id AS id_joya, COUNT(h.id) AS veces_fabricada
+                FROM joyas j
+                LEFT JOIN detalle_recetas dr ON j.id = dr.id_joya
+                LEFT JOIN tipos_componentes tc ON dr.id_componente = tc.id
+                LEFT JOIN historico_joyas h ON j.id = h.id_joya
+                GROUP BY j.id
+                HAVING MIN(tc.cantidad / dr.cantidad) >= 1
+                ORDER BY veces_fabricada');
+            }
+            if ($parametro == 1) {
+                $joyas = DB::select('SELECT j.id AS id_joya, COUNT(h.id) AS veces_fabricada
+                FROM joyas j 
+                LEFT JOIN detalle_recetas dr ON j.id = dr.id_joya
+                LEFT JOIN tipos_componentes tc ON dr.id_componente = tc.id
+                LEFT JOIN historico_joyas h ON j.id = h.id_joya
+                GROUP BY j.id
+                HAVING MIN(tc.cantidad / dr.cantidad) >= 1
+                ORDER BY veces_fabricada DESC');
+            }
+
+            for ($i = 0; $i < count($joyas); $i++) {
+                $joya = Joya::find($joyas[$i]->id_joya);
+                $disponibles[] = $joya;
+            }
+            return response()->json([$disponibles], 200);
+        } catch (Exception $e) {
+            return response()->json(['mensaje' => 'Error al obtener las joyas fabricables'], 500);
+        }
     }
 }
