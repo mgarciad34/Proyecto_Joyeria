c1187d293f28bcb7e5aacf58df2533bbda472ac5
Manuel Garcia <m.gd@outlook.es>
2023-12-08 19:02:24 +0100

    Añadida funcionalidad mostrar mapa

--- frontend/src/js/consultarLotesUsuario.js
+++ frontend/src/js/consultarLotesUsuario.js
@@ -1,5 +1,3 @@
-// http-consultarLotesUsuario.js
-
 import { obtenerDatos } from './http/http-consultarLotesUsuario.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     obtenerDatos(id)
         .then(responseArray => {
-            // Verifica si la respuestaArray tiene al menos un elemento
+            
             if (Array.isArray(responseArray) && responseArray.length > 0) {
                 const response = responseArray[0];
 
@@ -21,16 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
                         const fila = document.createElement('tr');
 
                         const columna1 = document.createElement('td');
-                        columna1.textContent = item.id; // Ajusta según la propiedad real en tu objeto
+                        columna1.textContent = item.id; 
                         fila.appendChild(columna1);
 
                         const columna2 = document.createElement('td');
-                        columna2.textContent = item.id_empresa; // Ajusta según la propiedad real en tu objeto
+                        columna2.textContent = item.id_empresa; 
                         fila.appendChild(columna2);
 
                         const columna3 = document.createElement('td');
                         columna3.textContent = item.estado;
                         fila.appendChild(columna3);
+
+                        const columnaBotones = document.createElement('td');
+
+                        const btnEditar = document.createElement('button');
+                        btnEditar.textContent = 'Mostrar Mapa';
+                        btnEditar.className = 'btn btn status cancelled';
+                        btnEditar.addEventListener('click', function(){
+                            // Abre el mapa en una nueva ventana o pestaña
+                            window.open('https://www.google.com/maps?q=' + item.latitud + ',' + item.longitud, '_blank');
+                        });
+
+                        columnaBotones.appendChild(btnEditar);
+
+                        const btnEliminar = document.createElement('button');
+                        btnEliminar.textContent = 'Cancelar Entrega';
+                        btnEliminar.className = 'btn btn status cancelled';
+                        columnaBotones.appendChild(btnEliminar);
+
+                        fila.appendChild(columnaBotones);
 
                         tbody.appendChild(fila);
                     });
