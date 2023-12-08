1e0ab061c54eab783f929a7d95d2506c7b10585f
Manuel Garcia <m.gd@outlook.es>
2023-12-08 12:38:20 +0100

    creando interfaz y funcionalidad js consultar lotes usuario

---
+++ frontend/src/js/consultarLotesUsuario.js
@@ -0,0 +1,55 @@
+//Manuel
+import { obtenerDatos } from "./http/http-consultarLotesUsuario.js";
+import { eliminarComponente } from "./http/http-eliminarComponente.js";
+import { crearCelda, crearBoton, mostrarModal } from "./http/http-tablaComponente.js";
+
+function mostrarDatosEnTabla() {
+    obtenerDatos(sessionStorage.getItem('token'))
+        .then(response => {
+            const data = response.tipos;
+            const tablaBody = document.getElementById('data');
+            tablaBody.innerHTML = '';
+            
+            for (let i = 0; i < data.length; i++) {
+                const item = data[i];
+                
+                const nuevaFila = document.createElement('tr');
+                
+                nuevaFila.id = `fila-${item.id}`;
+
+                for (const key in item) {
+                    //Hacemos que la ID no aparezca en la tabla
+                    if (key !== 'id') {
+                        nuevaFila.appendChild(crearCelda(item[key]));
+                    }
+                }
+
+                const btnModificar = crearBoton('Modificar', 'warning', () => {
+                    var btnGuardar = document.getElementById('btnGuardar');
+                    var btnCerrar = document.getElementById('btnCerrar');  
+
+                    mostrarModal(item, btnGuardar, btnCerrar);
+                });
+
+                const btnEliminar = crearBoton('Eliminar', 'danger', () => {
+                    eliminarComponente(item.id);
+                });
+
+                //Celdas de botones por fila
+                const celdaBotones = document.createElement('td');
+                celdaBotones.appendChild(btnModificar);
+                celdaBotones.appendChild(btnEliminar);
+                nuevaFila.appendChild(celdaBotones);
+
+                //Agregamos la fila a la tabla
+                tablaBody.appendChild(nuevaFila);
+            }
+        })
+        .catch(error => {
+            console.error('Error al obtener datos:', error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    mostrarDatosEnTabla();
+});
