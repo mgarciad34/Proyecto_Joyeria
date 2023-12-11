a27aeb6d642b1f7d7b5a55c6cf7c9860b5ca47fe
Manuel Garcia <m.gd@outlook.es>
2023-12-11 10:55:36 +0100

    añadida redireccion en las redirecciones

--- frontend/src/js/http/http-ObtenerRoles.js
+++ frontend/src/js/http/http-ObtenerRoles.js
@@ -1,25 +1,27 @@
-export function obtenerRoles(id) {
-    let url = `http://127.0.0.1:8000/api/roles/${id}`;
-    let resultado = {};
-
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error de red: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            resultado = data;
-            return resultado;
-        })
-        .catch(error => {
-            console.error('Error en la solicitud:', error);
-            throw error;
+//Manuel
+export async function obtenerRoles(id) {
+    try {
+        let token = sessionStorage.getItem('token')
+        let url = `http://127.0.0.1:8000/api/roles/${id}`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + token
+            },
         });
+
+        if (!response.ok) {
+            throw new Error(`Este usuario no tiene ningun rol asignado`);
+        }
+
+        if (response.status === 202) {
+            return 202;
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return 302;
+    }
 }
--- frontend/src/js/obtenerRoles.js
+++ frontend/src/js/obtenerRoles.js
@@ -1,65 +1,70 @@
 import { obtenerRoles } from "./http/http-ObtenerRoles.js";
 
 document.addEventListener('DOMContentLoaded', function () {
-    var rolesDiv = document.getElementById('roles');
+    var rolesDiv = document.getElementById('roles')
     if (rolesDiv) {
-        var id = sessionStorage.getItem("id-usuario");
+        var id = sessionStorage.getItem("id-usuario")
 
         obtenerRoles(id)
             .then(result => {
-                // Comprobamos que es un objeto con una clave numérica
-                if (result && typeof result === 'object' && Object.keys(result).length === 1) {
-                    // Obtener la primera clave numérica del objeto
-                    var id_usuario = Object.keys(result)[0];
+                if (result === 202) {
+                    return
+                } else if (result === 302) {
+                    return
+                } else {
 
-                    if (Array.isArray(result[id_usuario])) {
-                        result[id_usuario].forEach(rol => {
-                            const boton = document.createElement('button');
-                            boton.className = "btn status cancelled";
-                            switch (rol.id_rol) {
-                                case 1:
-                                    boton.textContent = "Administrador";
-                                    break;
-                                case 2:
-                                    boton.textContent = "Clasificador";
-                                    break;
-                                case 3:
-                                    boton.textContent = "Diseñador";
-                                    break;
-                                case 4:
-                                    boton.textContent = "Colaborador";
-                                    break;
-                            }
+                    if (result && typeof result === 'object' && Object.keys(result).length === 1) {
+                        var id_usuario = Object.keys(result)[0]
 
-                            boton.addEventListener('click', () => {
+                        if (Array.isArray(result[id_usuario])) {
+                            result[id_usuario].forEach(rol => {
+                                const boton = document.createElement('button')
+                                boton.className = "btn status cancelled"
                                 switch (rol.id_rol) {
                                     case 1:
-                                        window.location.href = './indexAdministrador.html';
-                                        break;
+                                        boton.textContent = "Administrador"
+                                        break
                                     case 2:
-                                        window.location.href = './indexClasificador.html';
-                                        break;
+                                        boton.textContent = "Clasificador"
+                                        break
                                     case 3:
-                                        window.location.href = './listaJoyas.html';
-                                        break;
+                                        boton.textContent = "Diseñador"
+                                        break
                                     case 4:
-                                        window.location.href = './IndexColaborador.html';
-                                        break;
+                                        boton.textContent = "Colaborador"
+                                        break
                                 }
-                            });
-                            rolesDiv.appendChild(boton);
-                        });
+
+                                boton.addEventListener('click', () => {
+                                    switch (rol.id_rol) {
+                                        case 1:
+                                            window.location.href = './indexAdministrador.html'
+                                            break
+                                        case 2:
+                                            window.location.href = './indexClasificador.html'
+                                            break
+                                        case 3:
+                                            window.location.href = './listaJoyas.html'
+                                            break
+                                        case 4:
+                                            window.location.href = './IndexColaborador.html'
+                                            break
+                                    }
+                                })
+                                rolesDiv.appendChild(boton)
+                            })
+                        } else {
+                            console.error('No es un array de roles:', result[id_usuario])
+                        }
                     } else {
-                        console.error('No es un array de roles:', result[id_usuario]);
+                        console.error('No es un objeto', result)
                     }
-                } else {
-                    console.error('No es un objeto', result);
                 }
             })
             .catch(error => {
-                console.error('Error al obtener roles:', error);
-            });
+                console.error('Error al obtener roles:', error)
+            })
     } else {
-        console.error('No se encontró el div de roles');
+        console.error('No se encontró el div de roles')
     }
-});
+})
