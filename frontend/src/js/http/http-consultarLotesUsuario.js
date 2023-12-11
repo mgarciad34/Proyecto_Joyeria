b21dc55eba2194974858f8bc746a86bf8a09b5d2
Manuel Garcia <m.gd@outlook.es>
2023-12-11 17:08:46 +0100

    Funcionalidad redireccion consultar lotes

--- frontend/src/js/http/http-consultarLotesUsuario.js
+++ frontend/src/js/http/http-consultarLotesUsuario.js
@@ -1,25 +1,32 @@
-//Manuel
-export function obtenerDatos(id) {
-    let url = `http://127.0.0.1:8000/api/lotes/entregados/${id}/`;
-    var token = sessionStorage.getItem('token');
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`, 
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
+// Manuel
+export async function obtenerDatos(id) {
+    try {
+        let token = sessionStorage.getItem('token');
+        let url = `http://127.0.0.1:8000/api/lotes/entregados/${id}/`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token,
+            },
+        });
+
+        if (!response.ok) {
+            if (response.status === 202) {
+                return 202;
+            } else {
                 throw new Error(`Error de red: ${response.status}`);
             }
-            return response.json();
-        })
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        if (error.message.includes('Error de red: 302')) {
+            return 302;
+        } else {
             console.error('Error en la solicitud:', error);
             throw error;
-        });
+        }
+    }
 }
