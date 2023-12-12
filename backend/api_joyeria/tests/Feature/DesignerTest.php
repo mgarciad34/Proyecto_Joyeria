d1998b75577a2739380ba634a9e62388c5da0642
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2023-12-12 12:51:47 +0100

    test actualizados

--- backend/api_joyeria/tests/Feature/DesignerTest.php
+++ backend/api_joyeria/tests/Feature/DesignerTest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 use App\Http\Controllers\ControladorJoya;
 use App\Models\Detalle_receta;
 use App\Models\Joya;
+use App\Models\RolAsignado;
+use App\Models\Tipos_componente;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
  
@@ -17,7 +19,15 @@ class DesignerTest extends TestCase
  /**Óscar */
     public function testNuevaJoyaExito()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
         $response = $this->withHeaders([
@@ -34,19 +44,31 @@ class DesignerTest extends TestCase
         $joyaId = $response->json('id');
         $joya = Joya::find($joyaId);
         $detalle = Detalle_receta::where('id_joya', '=', $joya->id)->delete();
+
         $joya->delete();
+        $usuario->delete();
+        $rolesAsignados->delete();
     }
      /**Óscar */
     public function testNuevaJoyaSinAcceso()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=2;
+        $rolesAsignados->save();
+    
         $token = $usuario->createToken('access_token', ['Clasificador'])->plainTextToken;
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->post('/api/joyas/nueva', [
             'nombre' => 'Joya de Prueba',
-            'id_usuario' => 3,
+            'id_usuario' => $usuario->id,
             'detalle' => [
                 ['tipo' => 1, 'cantidad' => 10],
                 ['tipo' => 2, 'cantidad' => 5],
@@ -54,17 +76,45 @@ class DesignerTest extends TestCase
         ]);
         $response->assertStatus(202);
         $response->assertJson(["success" => false, "message" => "No autorizado"]);
+        $usuario->delete();
+        $rolesAsignados->delete();
     }
  /**Óscar */
     public function testFabricarJoyaExito()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+
+        $tipo=new Tipos_componente();
+        $tipo->nombre='TestTipo';
+        $tipo->cantidad=1000;
+        $tipo->save();
+
+        $joya=new Joya();
+        $joya->nombre='Joya Test';
+        $joya->foto='testfoto';
+        $joya->id_usuario=$usuario->id;
+        $joya->save();
+        
+        $detalle=new Detalle_receta();
+        $detalle->id_componente=$tipo->id;
+        $detalle->id_joya=$joya->id;
+        $detalle->cantidad=1;
+
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+    
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
-        ])->put('/api/joyas/fabricar/1', [
-            'id_usuario' => 3,
+        ])->put('/api/joyas/fabricar/'.$joya->id, [
+            'id_usuario' => $usuario->id,
         ]);
 
 
@@ -73,11 +123,24 @@ class DesignerTest extends TestCase
         $response->assertJson([
             'mensaje' => 'Fabricado correctamente',
         ]);
+        $usuario->delete();
+        $rolesAsignados->delete();
+        $detalle->delete();
+        $tipo->delete();
+        $joya->delete();
     }
  /**Óscar */
     public function testFabricarJoyaInexistente()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
         $response = $this->withHeaders([
@@ -92,11 +155,22 @@ class DesignerTest extends TestCase
         $response->assertJson([
             'mensaje' => 'Error al fabricar la joya',
         ]);
+        $usuario->delete();
+        $rolesAsignados->delete();
+
     }
      /**Óscar */
     public function testGetAllJoyasExito()
-    {
-        $usuario = User::find(3);
+    {  $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
         $response = $this->withHeaders([
@@ -114,6 +188,8 @@ class DesignerTest extends TestCase
                 ],
             ]
         ]);
+        $usuario->delete();
+        $rolesAsignados->delete();
     }
      /**Óscar */
     public function testGetAllJoyasSinLogin()
@@ -128,7 +204,16 @@ class DesignerTest extends TestCase
  /**Óscar */
     public function testGetJoyaOfUsuarioExito()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
         $response = $this->withHeaders([
@@ -146,27 +231,53 @@ class DesignerTest extends TestCase
                 ],
             ]
         ]);
+        $usuario->delete();
+        $rolesAsignados->delete();
     }
      /**Óscar */
     public function testGetJoyaOfUsuarioSinJoyas()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
-        ])->get('/api/joyas/usuario/3');
+        ])->get('/api/joyas/usuario/'.$usuario->id);
         $response->assertStatus(200);
         $response->assertJsonStructure([]);
+        $usuario->delete();
+        $rolesAsignados->delete();
     }
  /**Óscar */
     public function testGetAllHistorialExito()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->get('/api/joyas/historial');
         $response->assertStatus(200);
+
+        $usuario->delete();
+        $rolesAsignados->delete();
+   
     }
      /**Óscar */
     public function testGetAllHistorialSinLogin()
@@ -177,20 +288,55 @@ class DesignerTest extends TestCase
  /**Óscar */
     public function testObtenerJoyaByIdExito()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+
+        $joya=new Joya();
+        $joya->nombre='Joya Test';
+        $joya->foto='testfoto';
+        $joya->id_usuario=$usuario->id;
+        $joya->save();
+        $detalle=new Detalle_receta();
+        $detalle->id_componente=1;
+        $detalle->id_joya=$joya->id;
+        $detalle->cantidad=1;
+
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
-        ])->get('/api/joyas/1');
+        ])->get('/api/joyas/'.$joya->id);
         $response->assertStatus(200);
         $response->assertJson([
-            'id' => 1,
+            'id' => $joya->id,
         ]);
+
+        $usuario->delete();
+        $rolesAsignados->delete();
+        $joya->delete();
+        $detalle->delete();
+
     }
      /**Óscar */
     public function testObtenerJoyaByIdSinExistir()
     {
-        $usuario = User::find(3);
+        $usuario = new User;
+        $usuario->name='Test';
+        $usuario->email='test@test.com';
+        $usuario->password='123456';
+        $usuario->save();
+        $rolesAsignados=new RolAsignado();
+        $rolesAsignados->id_usuario=$usuario->id;
+        $rolesAsignados->id_rol=3;
+        $rolesAsignados->save();
+
+      
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
@@ -199,19 +345,40 @@ class DesignerTest extends TestCase
         $response->assertJson([
             'mensaje' => 'Error al obtener la joya',
         ]);
+        $usuario->delete();
+        $rolesAsignados->delete();
     }
  /**Óscar */
     public function testUpdateJoyaExito()
 {
-    $usuario = User::find(3);
+    $usuario = new User;
+    $usuario->name='Test';
+    $usuario->email='test@test.com';
+    $usuario->password='123456';
+    $usuario->save();
+    $rolesAsignados=new RolAsignado();
+    $rolesAsignados->id_usuario=$usuario->id;
+    $rolesAsignados->id_rol=3;
+    $rolesAsignados->save();
+
+    $joya=new Joya();
+    $joya->nombre='Joya Test';
+    $joya->foto='testfoto';
+    $joya->id_usuario=$usuario->id;
+    $joya->save();
+    $detalle=new Detalle_receta();
+    $detalle->id_componente=1;
+    $detalle->id_joya=$joya->id;
+    $detalle->cantidad=1;
+
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
     $response = $this->withHeaders([
         'Authorization' => 'Bearer ' . $token,
     ])->put('/api/joyas/1', [
         'joya_original' => [
-            'nombre' => 'Pendientes',
-            'detalle' => [['id_componente' => 1, 'cantidad' => 1]],
+            'nombre' => $joya->nombre,
+            'detalle' => [['id_componente' => $joya->id_componente, 'cantidad' => $joya->cantidad]],
         ],
         'joya' => [
             'nombre' => 'Nuevo Nombre',
@@ -222,11 +389,24 @@ class DesignerTest extends TestCase
     $response->assertJson([
         'mensaje' => 'Actualizado correctamente',
     ]);
+
+    $usuario->delete();
+    $rolesAsignados->delete();
+    $joya->delete();
+    $detalle->delete();
 }
  /**Óscar */
 public function testUpdateJoyaSinDetalle()
 {
-    $usuario = User::find(3);
+    $usuario = new User;
+    $usuario->name='Test';
+    $usuario->email='test@test.com';
+    $usuario->password='123456';
+    $usuario->save();
+    $rolesAsignados=new RolAsignado();
+    $rolesAsignados->id_usuario=$usuario->id;
+    $rolesAsignados->id_rol=3;
+    $rolesAsignados->save();
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
     $response = $this->withHeaders([
@@ -254,39 +434,122 @@ public function testUpdateJoyaSinDetalle()
                 ]
         
     ]);
+    $usuario->delete();
+    $rolesAsignados->delete();
 }
  /**Óscar */
 public function testGetOwnerExito()
 {
-    $usuario = User::find(3);
+    $usuario = new User;
+    $usuario->name='Test';
+    $usuario->email='test@test.com';
+    $usuario->password='123456';
+    $usuario->save();
+    $rolesAsignados=new RolAsignado();
+    $rolesAsignados->id_usuario=$usuario->id;
+    $rolesAsignados->id_rol=3;
+    $rolesAsignados->save();
+
+    $joya=new Joya();
+    $joya->nombre='Joya Test';
+    $joya->foto='testfoto';
+    $joya->id_usuario=$usuario->id;
+    $joya->save();
+    $detalle=new Detalle_receta();
+    $detalle->id_componente=1;
+    $detalle->id_joya=$joya->id;
+    $detalle->cantidad=1;
+    
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
     $response = $this->withHeaders([
         'Authorization' => 'Bearer ' . $token,
-    ])->get('/api/joyas/owner/1/3');
+    ])->get('/api/joyas/owner/'.$joya->id.'/'.$usuario->id);
 
 
     $response->assertStatus(200);
 
     $response->assertJson(['resultado'=>true]);
+
+    $usuario->delete();
+    $rolesAsignados->delete();
+    $joya->delete();
+    $detalle->delete();
+    
 }
  /**Óscar */
 public function testGetOwnerError()
-{
-    $usuario = User::find(3);
-        $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
+{   
+    $usuario = new User;
+    $usuario->name='Test';
+    $usuario->email='test@test.com';
+    $usuario->password='123456';
+    $usuario->save();
+    $rolesAsignados=new RolAsignado();
+    $rolesAsignados->id_usuario=$usuario->id;
+    $rolesAsignados->id_rol=3;
+    $rolesAsignados->save();
+
+    $joya=new Joya();
+    $joya->nombre='Joya Test';
+    $joya->foto='testfoto';
+    $joya->id_usuario=$usuario->id;
+    $joya->save();
+    $detalle=new Detalle_receta();
+    $detalle->id_componente=1;
+    $detalle->id_joya=$joya->id;
+    $detalle->cantidad=1;
+
+    $usuario2 = new User;
+    $usuario2->name='Test2';
+    $usuario2->email='test2@test.com';
+    $usuario2->password='123456';
+    $usuario2->save();
+    
+        $token = $usuario2->createToken('access_token', ['Diseñador'])->plainTextToken;
 
     $response = $this->withHeaders([
         'Authorization' => 'Bearer ' . $token,
-    ])->get('/api/joyas/owner/1/1');
+    ])->get('/api/joyas/owner/'.$joya->id.'/'.$usuario2->id);
     $response->assertStatus(200);
 
     $response->assertJson(['resultado'=>false]);
+
+    $usuario->delete();
+    $usuario2->delete();
+    $rolesAsignados->delete();
+    $joya->delete();
+    $detalle->delete();
 }
  /**Óscar */
 public function testGetJoyasDisponibles()
 {
-    $usuario = User::find(3);
+    $usuario = new User;
+    $usuario->name='Test';
+    $usuario->email='test@test.com';
+    $usuario->password='123456';
+    $usuario->save();
+    $rolesAsignados=new RolAsignado();
+    $rolesAsignados->id_usuario=$usuario->id;
+    $rolesAsignados->id_rol=3;
+    $rolesAsignados->save();
+
+    $tipo=new Tipos_componente();
+    $tipo->nombre='TestTipo';
+    $tipo->cantidad=1000;
+    $tipo->save();
+
+    $joya=new Joya();
+    $joya->nombre='Joya Test';
+    $joya->foto='testfoto';
+    $joya->id_usuario=$usuario->id;
+    $joya->save();
+    $detalle=new Detalle_receta();
+    $detalle->id_componente=$tipo->id;
+    $detalle->id_joya=$joya->id;
+    $detalle->cantidad=1;
+
+   
         $token = $usuario->createToken('access_token', ['Diseñador'])->plainTextToken;
 
     $response = $this->withHeaders([
@@ -306,6 +569,11 @@ public function testGetJoyasDisponibles()
             ]
         ]
     ]);
+    $usuario->delete();
+    $rolesAsignados->delete();
+    $tipo->delete();
+    $joya->delete();
+    $detalle->delete();
 }
 
 }
