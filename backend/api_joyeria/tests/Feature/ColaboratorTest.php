fee253e8fec284653de580d86426ea9401d73ad1
Manuel Garcia <m.gd@outlook.es>
2023-12-12 18:47:43 +0100

    test lotes

--- backend/api_joyeria/tests/Feature/ColaboratorTest.php
+++ backend/api_joyeria/tests/Feature/ColaboratorTest.php
@@ -9,12 +9,63 @@ use Tests\TestCase;
 class ColaboratorTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Manuel
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+
+     //Test insertar lotes
+    public function testCrearLote()
+    {
+        $login = [
+            "email" => "colaborador@jawas.com",
+            "password" => "colaborador",
+        ];
+
+        $response = $this->json("post", "/api/login", $login);
+
+        $token = $response->json('token');
+
+
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post("/api/lotes/agregar/lote", [
+
+            "id_empresa" => 1,
+            "latitud" => "38",
+            "longitud" => "68,7",
+            "estado" => "Enviado"
+        ]);
+             $response->assertJson([
+                 "mensaje" => "Lote insertado correctamente",
+             ]);
     }
+
+
+     //Test consultar lotes
+     public function testConsultarLotes()
+     {
+         $adminLogin = [
+             "email" => "colaborador@jawas.com",
+             "password" => "colaborador",
+         ];
+
+         $response = $this->json("post", "/api/login", $adminLogin);
+         $token = $response->json('token');
+
+         $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get("/api/lotes/consultar");
+
+         $response->assertStatus(200)
+             ->assertJsonStructure([
+                 'mensaje' => [
+                     '*' => [
+                         'id',
+                         'id_empresa',
+                         'latitud',
+                         'longitud',
+                         'estado',
+                         'id_clasificador',
+                     ],
+                 ],
+             ]);
+     }
+
 }
