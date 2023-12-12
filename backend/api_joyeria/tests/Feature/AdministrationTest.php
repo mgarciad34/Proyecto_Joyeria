0e9606f4573096c2a8bd6f9cfebbfc273c7da247
Manuel Garcia <m.gd@outlook.es>
2023-12-12 12:52:19 +0100

    creando test

---
+++ backend/api_joyeria/tests/Feature/AdministrationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class AdministrationTest extends TestCase
+{
+    /**
+     * Manuel
+     */
+    //Test Registro de usuarios
+    public function testCrearUsuario()
+    {
+        $datos = [
+            "name" => "usuarioprueba",
+            "email" => "userprueba@correo.com",
+            "password" => "password123"
+        ];
+
+        $this->json("post", "/api/usuarios", $datos)
+            ->assertStatus(201)
+            ->assertJson([
+                "message" => "Usuario creado exitosamente",
+            ]);
+    }
+
+    // Test Login
+    public function testLogin()
+    {
+        $datos = [
+            "email" => "administrador@jawas.com",
+            "password" => "administrador"
+        ];
+
+        $this->json("post", "/api/login", $datos)
+            ->assertStatus(200)
+            ->assertJson([
+                "message" => "OK",
+            ]);
+    }
+}
